440999a28c82154b3af5543cbcb20a5543bd94ed
Victoria <victoriroly17@gmail.com>
2018-01-13 16:09:31 -0800

    Stopwatch
    added html file with pie chart

--- stopwatch.js
+++ stopwatch.js
@@ -4,8 +4,10 @@ window.onload = function () {
   
   var seconds = 00; 
   var tens = 00; 
+  var minutes = 00;
   var appendTens = document.getElementById("tens")
   var appendSeconds = document.getElementById("seconds")
+  var appendMinutes = document.getElementById("minutes");
   var buttonStart = document.getElementById('button-start');
   var buttonStop = document.getElementById('button-stop');
   var buttonReset = document.getElementById('button-reset');
@@ -26,8 +28,10 @@ window.onload = function () {
      clearInterval(Interval);
     tens = "00";
   	seconds = "00";
+	minutes = "00";
     appendTens.innerHTML = tens;
   	appendSeconds.innerHTML = seconds;
+	appendMinutes.innterHTML = minutes;
   }
   
    
@@ -55,6 +59,14 @@ window.onload = function () {
     if (seconds > 9){
       appendSeconds.innerHTML = seconds;
     }
+
+	if (seconds > 60) {
+	console.log("minutes");
+	minutes++;
+	appendMinutes.innerHTML = "0" + minutes;
+	seconds = 0;
+	appendSeconds.innerHTML = "0" + 0;
+	}
   
   }
   
